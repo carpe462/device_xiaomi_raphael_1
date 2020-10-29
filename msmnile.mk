1f9fad9560e43e3a0fc6461f0fbf97aabe475ab3
Adhitya Mohan <me@adhityamohan.in>
2020-10-29 10:54:29 +0530

    raphael: Build librmnetctl.so

--- msmnile.mk
+++ msmnile.mk
@@ -415,7 +415,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson \
     libril \
-    librilutils
+    librilutils \
+    librmnetctl
 
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-vendorcompat \
